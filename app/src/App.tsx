c35d6b8def50720df2b14bb4f2d94a5f6973b3bb
Alamelu.v <send2alamelu@gmail.com>
2021-10-30 10:40:57 +0800

    tech(base): add material-ui modules

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LoginPage from './pages/Login/Login';
-import DashboardPage from './pages/Dashboard/Dashboard';
-import TransactionPage from './pages/Transaction/Transaction';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import './App.css';
+
+const LoginPage = React.lazy(() => import('./pages/Login/Login' /* webpackChunkName: "Login" */));
+const DashboardPage = React.lazy(() => import('./pages/Dashboard/Dashboard' /* webpackChunkName: "Dashboard" */));
+const TransactionPage = React.lazy(() => import('./pages/Transaction/Transaction' /* webpackChunkName: "Transaction" */));
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/transaction" component={TransactionPage} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Container maxWidth="sm">
+              <Box sx={{ my: 4 }}>
+                <Route exact path="/" component={LoginPage} />
+                <Route path="/dashboard" component={DashboardPage} />
+                <Route path="/transaction" component={TransactionPage} />
+              </Box>
+            </Container>
+          </Suspense>
         </Switch>
       </BrowserRouter>
     </React.Fragment>
--- app/src/pages/Login/Login.tsx
+++ app/src/pages/Login/Login.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import Link from '@mui/material/Link';
 import { useHistory } from 'react-router-dom';
 
 function Login() {
   const history = useHistory();
-  const onClick = (e: any) => {
+  const goToDashboard = (e: any) => {
     e.preventDefault();
     history.push("/dashboard");
   }
   return (
     <div className="Login">
       Login Form Goes here...
-      <a href="#" onClick={onClick}>Go to dashboard</a>
+      <Link color="inherit" href="https://mui.com/" onClick={goToDashboard}>
+        Go to dashboard
+      </Link>
     </div>
   );
 }
