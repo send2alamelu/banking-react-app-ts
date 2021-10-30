f36f8f6582c7117f0828d539ed5ae1888b970ed1
Alamelu.v <send2alamelu@gmail.com>
2021-10-30 12:56:47 +0800

    feat(login): add base

--- app/src/pages/Login/Login.tsx
+++ app/src/pages/Login/Login.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import Link from '@mui/material/Link';
 import { useHistory } from 'react-router-dom';
 
+import TextInput from '../../Components/Fields/TextInput/TextInput';
+import PasswordInput from '../../Components/Fields/PasswordInput/PasswordInput';
+import ActionButton from '../../Components/Fields/ActionButton/ActionButton';
+
 function Login() {
   const history = useHistory();
-  const goToDashboard = (e: any) => {
+  const goToDashboard = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     history.push("/dashboard");
   }
+
   return (
     <div className="Login">
-      Login Form Goes here...
-      <Link color="inherit" href="https://mui.com/" onClick={goToDashboard}>
-        Go to dashboard
-      </Link>
+      <TextInput label="Username" />
+      <br />
+      <PasswordInput label="Password" />
+      <br /><br />
+      <ActionButton onClick={goToDashboard} />
     </div>
   );
 }
