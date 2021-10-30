1711d8b375a1a274a3aa36d9321ae589aaeeaee4
Alamelu.v <send2alamelu@gmail.com>
2021-10-30 12:57:33 +0800

    feat(base): add base modules and update App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
 import './App.css';
 
 const LoginPage = React.lazy(() => import('./pages/Login/Login' /* webpackChunkName: "Login" */));
@@ -13,7 +14,7 @@ function App() {
     <React.Fragment>
       <BrowserRouter>
         <Switch>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<CircularProgress />}>
             <Container maxWidth="sm">
               <Box sx={{ my: 4 }}>
                 <Route exact path="/" component={LoginPage} />
