c353444c6037c2cb965bf0fce455654bd7a778ed
Alamelu.v <send2alamelu@gmail.com>
2021-11-02 20:17:56 +0800

    fix(bottomNav): update bottom navigation

--- app/src/components/Common/BottomNav/BottomNav.tsx
+++ app/src/components/Common/BottomNav/BottomNav.tsx
@@ -1,20 +1,11 @@
-import { useState } from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavProps from './BottomNav.props';
 
 export default function BottomNav(props: BottomNavProps) {
-  const [value, setValue] = useState(0);
-
   return (
     <Box pt={4}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
+      <BottomNavigation showLabels>
         {props?.children}
       </BottomNavigation>
     </Box>
